56257cfd7dc5b6d776bd4bd444fa61317e4c9138
Anirudha Kapileshwari <akapileshwari@hawk.iit.edu>
2025-04-10 12:22:27 -0500

    image upodates

--- src/components/SliderSection.jsx
+++ src/components/SliderSection.jsx
@@ -6,16 +6,16 @@ import styles from './SliderSection.module.scss';
 
 const slider1 = [
   { color: "#e3e5e7", src: "15.JPG" },
-  { color: "#d6d7dc", src: "18.jpg" },
+  { color: "#d6d7dc", src: "18.JPG" },
   { color: "#e3e3e3", src: "17.jpg" },
-  { color: "#21242b", src: "19.jpg" }
+  { color: "#21242b", src: "19.JPG" }
 ];
 
 const slider2 = [
-  { color: "#d4e3ec", src: "21.jpg" },
+  { color: "#d4e3ec", src: "21.JPG" },
   { color: "#e5e0e1", src: "16.JPG" },
-  { color: "#d7d4cf", src: "20.jpg" },
-  { color: "#e1dad6", src: "22.jpg" }
+  { color: "#d7d4cf", src: "20.JPG" },
+  { color: "#e1dad6", src: "22.JPG" }
 ];
 
 export default function SliderSection() {
