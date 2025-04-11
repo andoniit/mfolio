6070925f46c02f728b5fe67b25df9d394d2b17cc
Anirudha Kapileshwari <akapileshwari@hawk.iit.edu>
2025-04-11 10:20:02 -0500

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export const metadata = {
   description: 'Potefolio website of me ',
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,12 +27,26 @@ export default function RootLayout({
     
       <html lang="en" className={`${tajawal.variable} ${shadows.variable}`}>
       <head>
+        
         {/* Boldonse must be added via <link> because it's not supported by next/font */}
         <link
           href="https://fonts.googleapis.com/css2?family=Boldonse&display=swap"
           rel="stylesheet"
         />
-
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1N7X8DBHKY"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', 'G-1N7X8DBHKY');
+            `,
+          }}
+        />
       </head>
       
       <body>  {children}</body>
